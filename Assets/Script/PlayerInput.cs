dd8e6f1782da731a4125fcf3fa3063bb73dd6843
전인재 <aa@aa.com>
2020-04-26 16:01:14 +0900

    테스트

--- Assets/Script/PlayerInput.cs
+++ Assets/Script/PlayerInput.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+
     //케릭터 행동 입력값(유니티 Axes)
     public string moveAxis = "Vertical";
     public string rotateAxis = "Horizontal";
@@ -23,8 +24,5 @@ public class PlayerInput : MonoBehaviour
         rotate = Input.GetAxis(rotateAxis);
         attack1 = Input.GetButton(attack1Button);
         attack2 = Input.GetButton(attack2Button);
-        
-        
-
     }
 }
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-
     //움직임,회전 속도, 점프 강도
     public float moveSpeed = 2f;
     public float rotateSpeed = 200f;
@@ -14,14 +12,11 @@ public class PlayerMovement : MonoBehaviour
     //케릭터 점프 횟수
     private int jumpCount = 0;
     
-
-    
     //각 컴퍼넌트 변수선언
     private PlayerInput playerInput;
     private Rigidbody playerRigidbody;
     private Animator playerAnimator;
 
-    
 
     void Start()
     {
@@ -87,25 +82,18 @@ public class PlayerMovement : MonoBehaviour
     //바닥 접촉 체크,점프횟수 초기화
     void OnCollisionEnter(Collision collision)
     {
-
-        
         //첫번째 접촉 면적의 y각도 값이 0.7 이상이면(각도가 완만하면) 점프 횟수 초기화
         if (collision.contacts[0].normal.y > 0.7f)
         {
             jumpCount = 0;
             playerAnimator.SetBool("Jump", false);
             Debug.Log(jumpCount);
-
         }
     }
 
     void Attack1()
     {
-        if(playerInput.attack1 == true)
-        {
-            playerAnimator.SetBool("Attack1", true);
-        }
-        
+        playerAnimator.SetBool("Attack1", playerInput.attack1);        
     }
 
 
