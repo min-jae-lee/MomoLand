e5268c42b496b094863d0c3318e737836c7eca54
min-jae-lee <parsley701@gmail.com>
2020-05-08 12:02:04 +0900

    몬스터 랜덤 좌표 자동 순찰 구현중

--- Assets/Script/TurtleShell.cs
+++ Assets/Script/TurtleShell.cs
@@ -1,49 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
 //Monster 상속
 public class TurtleShell : Monster
-{ 
+{
     public override string Name { get => "거북이"; }
-    
     private Transform _transform;
     Vector3 targetPos;
-    
+    Vector3 targetLook;
+
 
     protected override void Start()
     {
         base.Start();
-
         _transform = GetComponent<Transform>();
-        float posY = _transform.position.y;
-        float ranX = Random.Range(-0.5f, 0.5f);
-        float ranZ = Random.Range(-0.5f, 0.5f);
-        //targetPos = new Vector3(_transform.position.x+ranX, posY, _transform.position.z+ranZ);
-        targetPos = new Vector3(_transform.position.x + -1f, posY, _transform.position.z + 1f);        
-        Debug.Log(_transform.position);
-        Debug.Log(targetPos);
-        
+        PosSet();
 
     }
+
+
 
     void Update()
     {
         MoveTest();
-
     }
+
+    void PosSet()
+    {
+        float posY = _transform.position.y;
+        float ranX = Random.Range(-0.3f, 0.3f);
+        float ranZ = Random.Range(-0.3f, 0.3f);
+        targetPos = new Vector3(_transform.position.x + ranX, posY, _transform.position.z + ranZ);
+        targetLook = targetPos - _transform.position;
+    }
+
 
     void MoveTest()
     {
-        
-        _transform.position = Vector3.MoveTowards(_transform.position, targetPos, 0.02f);
- 
+
+        _transform.position = Vector3.MoveTowards(_transform.position, targetPos, 0.01f);
+        _transform.rotation = Quaternion.LookRotation(targetLook);
+
 
     }
 
 
-    
+
 
 
 
