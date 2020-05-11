122aec4bcfeda91a681893e2b8ae0c96e229173c
min-jae-lee <parsley701@gmail.com>
2020-05-12 00:36:01 +0900

    몬스터 반응 범위 안에 플레이어 들어올시 느낌표 스프라이트 팝업하기 구현완료

---
+++ Assets/Script/ExclamationCollider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExclamationCollider : MonoBehaviour
+{
+    public Image exclamation;
+ 
+
+    void Start()
+    {
+        exclamation.enabled = false;
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(Exclamation());
+        }
+    }
+
+    IEnumerator Exclamation()
+    {
+        exclamation.enabled = true;
+        yield return new WaitForSeconds(1f);
+        exclamation.enabled = false;
+    }
+
+
+
+}
