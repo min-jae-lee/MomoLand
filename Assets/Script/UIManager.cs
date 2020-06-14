7f682886b4d67014ffef94a51cbdba2aabf3eef3
min-jae-lee <parsley701@gmail.com>
2020-06-14 19:14:55 +0900

    보스방진입후 사망시 재시작 지점 선택(처음 or 보스방입구)할 수 있게 구현완료

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -12,25 +12,28 @@ public class UIManager : MonoBehaviour
     public Player playerCs;
     public GameObject gameOverUI;
     public Animator playerAnimator;
+    public GameObject buttonBoss;
 
+
+    void Update()
+    {
+        buttonBoss.SetActive(enterBoss);
+    }
 
     public void GameRestart()
     {
         //사망시 일반적인 재시작
-        if(enterBoss == false)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-
-        //플레이어가 보스방 진입후 사망시 보스방 입구에서 재시작 (HP는 70회복)
-        if(enterBoss == true)
-        {
-            player.transform.position = new Vector3(-3.5f, 0.5f, 23.2f);
-            playerCs.dead = false;
-            playerCs.curHp = 70;
-            playerAnimator.SetTrigger("Revival");
-            gameOverUI.SetActive(false);
-        }    
-        
+    }
+    //보스재도전(보스방 입구에서 재시작)
+    public void GameRestartBoss()
+    {
+        player.transform.position = new Vector3(-3.5f, 0.5f, 23.2f);
+        playerCs.dead = false;
+        playerCs.curHp = 70;
+        playerAnimator.SetTrigger("Revival");
+        gameOverUI.SetActive(false);
     }
 }
