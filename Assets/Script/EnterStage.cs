a139d108f31eafce3d574ca04885c090bd91eb6c
min-jae-lee <parsley701@gmail.com>
2020-06-21 00:40:47 +0900

    스테이지마다 다른 BGM 적용중

---
+++ Assets/Script/EnterStage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterStage : MonoBehaviour
+{
+    public AudioClip stage2Clip;
+    public AudioClip stage3Clip;
+    public AudioClip stage4Clip;
+    public AudioClip BossClip;
+    public AudioSource audioSource;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(gameObject.tag == "Stage2")
+            {
+                audioSource.clip = stage2Clip;
+                audioSource.Play();
+                Destroy(gameObject);
+            }
+            if (gameObject.tag == "Stage3")
+            {
+                audioSource.clip = stage3Clip;
+                audioSource.Play();
+                Destroy(gameObject);
+            }
+            if (gameObject.tag == "Stage4")
+            {
+                audioSource.clip = stage3Clip;
+                audioSource.Play();
+                Destroy(gameObject);
+            }
+            if (gameObject.tag == "Stage5")
+            {
+                audioSource.clip = BossClip;
+                audioSource.Play();
+                Destroy(gameObject);
+            }
+        }
+            
+    }
+}
