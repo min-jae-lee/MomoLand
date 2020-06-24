adc98e7c8010034d0953c2214e3192b0c8090299
min-jae-lee <parsley701@gmail.com>
2020-06-24 12:08:07 +0900

    모바일, PC 전환 버튼 구현중

---
+++ Assets/Script/MobileManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileManager : MonoBehaviour
+{
+    public Button buttonMobile;
+    public Button buttonPc;
+    public GameObject joystickUI;
+
+    public void MobileOn()
+    {
+        buttonMobile.enabled = false;
+        buttonPc.enabled = true;
+    }
+
+    public void PcOn()
+    {
+        buttonPc.enabled = false;
+        buttonMobile.enabled = true;
+    }
+
+}
